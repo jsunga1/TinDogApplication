2086207f18bb557974fdc738c5065167d4214b79
jsunga1 <jeanel.sunga14@stjohns.edu>
2017-04-30 22:11:23 -0400

    New User Class Fixed
    New User is functioning, still needs to be hooked to backend

--- src/NewUserFrame.java
+++ src/NewUserFrame.java
@@ -8,15 +8,13 @@ import java.awt.GridLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.*;
 
-<<<<<<< HEAD
 public class NewUserFrame extends JFrame{
 	private static final int FRAME_WIDTH = 400;
 	private static final int FRAME_HEIGHT = 300;
 	private static final int FIELD_WIDTH = 10;
-	private JLabel nameLabel;
-	private JTextField nameField;
-	private JLabel emailLabel;
+	/*private JLabel emailLabel;
 	private JTextField emailField;
 	private JLabel passwordLabel;
 	private JTextField passwordField;
@@ -25,7 +23,15 @@ public class NewUserFrame extends JFrame{
 	private JLabel phoneLabel;
 	private JTextField phoneField;
 	private JButton cancelButton;
-	private JButton submitButton;
+	private JButton submitButton;*/
+	
+	private JPanel contentPane;
+	private JTextField textFieldFirstName;
+	private JTextField textFieldEmail;
+	private JTextField textFieldPassword;
+	private JTextField textFieldPasswordConfirmation;
+	private JTextField textFieldPhoneNumber;
+	private JTextField textFieldLastName;
 	
 	private ActionListener cancelButtonListener;
 	private ActionListener submitButtonListener;
@@ -35,84 +41,7 @@ public class NewUserFrame extends JFrame{
 	private JPanel southPanel;
 	
 	public NewUserFrame(){
-		class cancelListener implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-				JFrame frameStartFrame = new StartFrame();
-				close();
-				frameStartFrame.setVisible(true);
-				frameStartFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			}
-		}
-		class submitListener implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-				try{
-					UniversalDogDB db = new UniversalDogDB();
-					String query = "select * from USER where USER_Email = \"" + emailField.setText(t);;
-					
-					db.sendData(query);
-				}catch (Exception newUserFailed)
-				{
-					System.out.println(newUserFailed);
-				}
-			}
-		}
-		createComponents();
-		cancelButtonListener = new cancelListener();
-		cancelButton.addActionListener(cancelButtonListener);
-		submitButtonListener = new submitListener();
-		submitButton.addActionListener(submitButtonListener);
-		createPanel();
-		setSize(FRAME_WIDTH, FRAME_HEIGHT);
-	}
-	public void createComponents(){
-		nameLabel = new JLabel("Name:");
-		nameField = new JTextField(FIELD_WIDTH);
-		emailLabel = new JLabel("Email:");
-		emailField = new JTextField(FIELD_WIDTH);
-		passwordLabel = new JLabel("Password:");
-		passwordField = new JTextField(FIELD_WIDTH);
-		passwordConfirmLabel = new JLabel("Password Confirmation:");
-		passwordConfirmField = new JTextField(FIELD_WIDTH);
-		phoneLabel = new JLabel("Phone Number:");
-		phoneField = new JTextField(FIELD_WIDTH);
-		cancelButton = new JButton("Cancel");
-		submitButton = new JButton("Submit");
-	}
-	public void createPanel(){
-		panel = new JPanel();
-		panel.setLayout(new BorderLayout());
-		westPanel = new JPanel();
-		westPanel.setLayout(new GridLayout(10,1));
-		southPanel = new JPanel();
-		westPanel.add(nameLabel);
-		westPanel.add(nameField);
-		westPanel.add(emailLabel);
-		westPanel.add(emailField);
-		westPanel.add(passwordLabel);
-		westPanel.add(passwordField);
-		westPanel.add(passwordConfirmLabel);
-		westPanel.add(passwordConfirmField);
-		westPanel.add(phoneLabel);
-		westPanel.add(phoneField);
-		panel.add(westPanel, BorderLayout.CENTER);
-		southPanel.add(cancelButton);
-		southPanel.add(submitButton);
-		panel.add(southPanel, BorderLayout.SOUTH);
-		add(panel);
-	}
-	public void close(){
-		this.setVisible(false);
-=======
-public class NewUserFrame extends JFrame {
-
-	private JPanel contentPane;
-	private JTextField textFieldFirstName;
-	private JTextField textFieldEmail;
-	private JTextField textFieldPassword;
-	private JTextField textFieldPasswordConfirmation;
-	private JTextField textFieldPhoneNumber;
-	private JTextField textFieldLastName;
-	public NewUserFrame() {
+	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -180,7 +109,70 @@ public class NewUserFrame extends JFrame {
 		
 		JButton btnSubmit = new JButton("Submit");
 		panelSouth.add(btnSubmit);
->>>>>>> origin/master
+		
+		class cancelListener implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				JFrame frameStartFrame = new StartFrame();
+				close();
+				frameStartFrame.setVisible(true);
+				frameStartFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			}
+		}
+		class submitListener implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				/*try{
+					UniversalDogDB db = new UniversalDogDB();
+					String query = "select * from USER where USER_Email = \"" + emailField.getText();
+					
+					db.sendData(query);
+				}catch (Exception newUserFailed)
+				{
+					System.out.println(newUserFailed);
+				}*/
+				textFieldEmail.setText("This works");
+			}
+		}
+		cancelButtonListener = new cancelListener();
+		btnCancel.addActionListener(cancelButtonListener);
+		submitButtonListener = new submitListener();
+		btnSubmit.addActionListener(submitButtonListener);
+	
+		setSize(FRAME_WIDTH, FRAME_HEIGHT);
 	}
-
+	/*public void createComponents(){
+		
+		emailLabel = new JLabel("Email:");
+		emailField = new JTextField(FIELD_WIDTH);
+		passwordLabel = new JLabel("Password:");
+		passwordField = new JTextField(FIELD_WIDTH);
+		passwordConfirmLabel = new JLabel("Password Confirmation:");
+		passwordConfirmField = new JTextField(FIELD_WIDTH);
+		phoneLabel = new JLabel("Phone Number:");
+		phoneField = new JTextField(FIELD_WIDTH);
+		cancelButton = new JButton("Cancel");
+		submitButton = new JButton("Submit");
+	}
+	public void createPanel(){
+		panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		westPanel = new JPanel();
+		westPanel.setLayout(new GridLayout(10,1));
+		southPanel = new JPanel();
+		westPanel.add(emailLabel);
+		westPanel.add(emailField);
+		westPanel.add(passwordLabel);
+		westPanel.add(passwordField);
+		westPanel.add(passwordConfirmLabel);
+		westPanel.add(passwordConfirmField);
+		westPanel.add(phoneLabel);
+		westPanel.add(phoneField);
+		panel.add(westPanel, BorderLayout.CENTER);
+		southPanel.add(cancelButton);
+		southPanel.add(submitButton);
+		panel.add(southPanel, BorderLayout.SOUTH);
+		add(panel);
+	}*/
+	public void close(){
+		this.setVisible(false);
+	}
 }
