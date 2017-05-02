a351141dfa3d6392da3a15dd819745dbdd3f8fa1
jsunga1 <jeanel.sunga14@stjohns.edu>
2017-05-02 19:20:09 -0400

    Revised USER class and added new user to startframe and new user frame

--- src/User.java
+++ src/User.java
@@ -5,94 +5,68 @@ import java.util.*;
 public class User
 {
 	private DoggieBag db;
-	private String name;
+	private String firstName;
+	private String lastName;
 	private String email;
 	private String phone;
 	private String password;
-	private int id;
 	private UniversalDogDB udb;
 	
 	public User()
 	{
 		db = new DoggieBag();
-		name = "User";
+		firstName = "User";
+		lastName = "User";
 		email = "User";
 		phone = "User";
 		password = "User";
 		
 	}
 	
-	public void setDogInfo(String UserName)
+	public void setUserInfo(String UserName)
 	{
 		try{
 			udb = new UniversalDogDB();
-			String q = "select * from USER where USER_Email = " + UserName;
+			String q = "select * from USER where USER_Email = \"" + UserName + "\";";
 			udb.retrieveData(q);
 			ResultSet rs = udb.getResultSet();
 			
 			if (rs.next())
 			{
 				email = UserName;
-				name = rs.getString("USER_Name");
-				phone = rs.getString("USER_Phone");
+				firstName = rs.getString("USER_First_Name");
+				lastName = rs.getString("USER_Last_Name");
+				phone = rs.getString("USER_Phone_Number");
 				password = rs.getString("USER_Password");
-				id = rs.getInt("USER_ID");
-				//db = rs.getString("DOG_Description");
-				//dogPic = rs.getString("DOG_Picture");
 			}
 			
-		}catch(Exception dogFail)
+		}catch(Exception userFail)
 		{
-			System.out.println(dogFail);
+			System.out.println(userFail);
 		}
 	}
 	
-	public void setName(String name)
+
+	public String getFirstName()
 	{
-		this.name = name;
+		return firstName;
 	}
-	public String getName()
+	public String getLastName()
 	{
-		return name;
-	}
-	
-	public void setEmail(String email)
-	{
-		this.email = email;
+		return lastName;
 	}
 	public String getEmail()
 	{
 		return email;
 	}
 	
-	public void setPhone(String phone)
-	{
-		this.phone = phone;
-	}
 	public String getPhone()
 	{
 		return phone;
 	}
 	
-	public void setPassword(String password)
-	{
-		this.password = password;
-	}
 	public String getPassword()
 	{
 		return password;
-	}
-	public void setID(int id)
-	{
-		this.id = id;
-	}
-	public int getID()
-	{
-		return id;
-	}
-	
-	public void addDog(Dog d)
-	{
-		db.addDog(d);
 	}
 }
